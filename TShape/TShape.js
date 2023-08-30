195c97e576963cf19441cbe898bcccf9f0ae5bcb
sarathorhalls <sarathorhalls@gmail.com>
2023-08-30 13:15:15 +0000

    added tshape drawn with triangle_strip

---
+++ TShape/TShape.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var gl;
+var points;
+
+window.onload = function init()
+{
+    //
+    // Init WebGL
+    //
+    var canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    var vertices = [
+        vec2( 0.2, -0.8),
+        vec2(-0.2, -0.8),
+        vec2( 0.2,  0.4),
+        vec2(-0.2,  0.4),
+        vec2(-0.8,  0.8),
+        vec2(-0.8,  0.4),
+        vec2( 0.8,  0.8),
+        vec2( 0.8,  0.4)
+    ]
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    //  Load shaders and initialize attribute buffers
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // Load the data into the GPU
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 8);
+}
